4cd544240e9a21c8deff60f6cc8d3330c53d9018
Raja28 <rajadavid03@gmail.com>
2025-04-23 16:41:02 +0530

    fix: routes path

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,10 +2,10 @@
 const BASEURL = import.meta.env.VITE_BASE_URL
 
 
-export const GETALLDETAILS = BASEURL + '/all-leads'
-export const ADDAGENT = BASEURL + '/add-agent'
-export const ADDLEAD = BASEURL + '/add-lead'
-export const UPDATELEAD = BASEURL + '/update-lead'
-export const ADDCOMMENT = BASEURL + '/add-comment'
-export const ADDNEWTAG = BASEURL + '/add-tag'
-export const GET_ALL_AGENT = BASEURL + '/all-agents'
+export const GETALLDETAILS = BASEURL + '/lead/all-leads'
+export const ADDAGENT = BASEURL + '/lead/add-agent'
+export const ADDLEAD = BASEURL + '/lead/add-lead'
+export const UPDATELEAD = BASEURL + '/lead/update-lead'
+export const ADDCOMMENT = BASEURL + '/lead/add-comment'
+export const ADDNEWTAG = BASEURL + '/lead/add-tag'
+export const GET_ALL_AGENT = BASEURL + '/lead/all-agents'
